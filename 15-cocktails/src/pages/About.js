a341917a0ad96ae520fb071237a1841f6915d9ca
Daniel <danielwebdeveloper220@gmail.com>
2021-01-01 10:22:01 -0500

    react basic projects

---
+++ 15-cocktails/src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt est
+        autem debitis omnis blanditiis eius repudiandae expedita, recusandae
+        itaque animi tempora natus esse excepturi, tenetur, eum vitae alias
+        architecto rerum ipsum assumenda! Placeat, sint? Aspernatur a
+        accusantium aut voluptate id!
+      </p>
+    </section>
+  );
+};
+
+export default About;
